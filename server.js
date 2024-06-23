7871fc05fec12f34b2b65007dd8b6e3788bc1c77
ahmed <ahmadghnnam60@gmail.com>
2024-06-23 19:33:14 +0300

    final commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 app.use(cors({
     credentials: true,
-    origin: '*', //host name of the client URL,
+    origin: process.env.CLIENT_URL, //host name of the client URL,
 
 }));
 
